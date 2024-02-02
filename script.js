8070633f3b25ec7246a76249c19f16f947ccd1ea
Annaliza_C <aldcadiente@gmail.com>
2024-02-01 16:14:36 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Exercise 1
+var numberOfHorses = 25;
+
+console.log(numberOfHorses);
+
+//Exercise 2
+var favoriteFlower = "roses";
+
+console.log("My favorite flower is " + favoriteFlower);
+
+//Exercise 3
+var city = "San Francisco";
+var birthday = "October 27, 1982";
+
+console.log("I was born in " + city + " on " + birthday);
